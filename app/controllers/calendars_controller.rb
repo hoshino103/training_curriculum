e01a33a2a337d1ebfe803df3f8c23e4d7acafecc
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-09-19 11:25:03 +0900

    issue1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,7 +23,7 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例)　今日が2月1日の場合・・・ Date.today.day: 1日
 
     @week_days = []
 
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
